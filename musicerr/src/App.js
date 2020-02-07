2d5e50504a90d81298960ed02f8fbe4abdb638d6
Postimal <michal.postek1@wp.pl>
2020-02-07 14:01:00 +0100

    some feature

--- musicerr/src/App.js
+++ musicerr/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [param, setParams] = useState("");
+  const [param, setParams] = useState('');
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const [data, setData] = useState([]);
 
   const handleChange = e => {
@@ -14,20 +15,31 @@ function App() {
     setLoading(true);
     try {
       let response = await fetch(
-        `https://cors-anywhere.herokuapp.com/https://www.songsterr.com/a/ra/songs.json?pattern=${param}`
+        // `https://cors-anywhere.herokuapp.com/https://www.songsterr.com/a/ra/songs.json?pattern=${param}&size=100`
+        `https://cors-anywhere.herokuapp.com/https://www.songsterr.com/api/songs?pattern=${param}&size=200`
       );
       let data = await response.json();
       console.log(data);
       setData(data.slice(0, 20));
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.log(error.message);
+      setError(error.message);
     }
   };
 
+  if (loading) {
+    return <div>ładuje...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="App">
-      hi
-      <form action="" onClick={search} className="searchTab">
+      <form action="" className="searchTab">
         <div className="searchTabs" id="searchBar">
           <label htmlFor="tabSearch"></label>
           <input
@@ -36,14 +48,34 @@ function App() {
             name="enteredTab"
             onChange={handleChange}
             value={param}
-            placeholder="Search for guitar tabs here"
+            placeholder="Search for artist or song"
             autoComplete="off"
           />
-          <button type="button" className="magnify">
+          <button onClick={search} type="button" className="magnify">
             <span className="sr-only">search</span>
           </button>
         </div>
       </form>
+      <div>
+        {data.map((item, i) => (
+          <div className="music-item" key={i}>
+            <h3>
+              {i} title:{item.title}
+            </h3>
+            <h4>{item.artist.nameWithoutThePrefix}</h4>
+            {item.tabTypes.map(tab => (
+              <a
+                key={tab}
+                target="_blank"
+                href={`http://www.songsterr.com/a/wa/bestMatchForQueryString?s=${item.title}&a=${item.artist.nameWithoutThePrefix}`}
+                // http://www.songsterr.com/a/wa/bestMatchForQueryString?s=Paganini 5th Caprice Crossroads&a=Steve Vai&track=drums
+              >
+                ${tab} ***
+              </a>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
